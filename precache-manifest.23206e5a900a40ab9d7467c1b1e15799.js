bafb3f16bd13b1b627032f10fa0db9b70bd09e75
mreckless <michaelreckless@hotmail.com>
2020-04-05 13:02:20 +0100

    Updates

---
+++ precache-manifest.23206e5a900a40ab9d7467c1b1e15799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3e2c65ef4bb7450c50f6c7ee1a28d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73885caf9b3838c258ce",
+    "url": "/static/css/main.f8e5f4d5.chunk.css"
+  },
+  {
+    "revision": "7fc5373bd7a5dd260bc4",
+    "url": "/static/js/2.6b965898.chunk.js"
+  },
+  {
+    "revision": "dad561af0568c79a501381148643f9d1",
+    "url": "/static/js/2.6b965898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73885caf9b3838c258ce",
+    "url": "/static/js/main.ff3f5850.chunk.js"
+  },
+  {
+    "revision": "f41e9488ad8332719dfd",
+    "url": "/static/js/runtime-main.efcdf6d6.js"
+  }
+]);
