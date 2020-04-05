1f1dddbb8442d67f628a2fdd0b264ce24b2cfd90
mreckless <michaelreckless@hotmail.com>
2020-04-05 13:20:44 +0100

    Split out carousel component into seperate component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { useState } from 'react'
+import ItemsCarousel from 'react-items-carousel'
+
+const style = {
+    arrow: {
+        width: 50,
+        userSelect: 'none',
+        opacity: 0.25,
+        transition: 'opacity 0.2s'
+    },
+    imageContainer: {
+        width: 200,
+        height: 290
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover'
+    }
+}
+
+export const Carousel = (props) => {
+    const [activeItemIndex, setActiveItemIndex] = useState(0)
+    const { images } = props
+    const [leftOpacity, setLeftOpacity] = useState(0.25)
+    const [rightOpacity, setRightOpacity] = useState(0.25)
+
+    const toggleLeftOpacity = () => {
+        setLeftOpacity(prevOpacity => prevOpacity === 0.25 ? 1 : 0.25)
+    }
+
+    const toggleRightOpacity = () => {
+        setRightOpacity(prevOpacity => prevOpacity === 0.25 ? 1 : 0.25)
+    }
+
+    return (
+        <ItemsCarousel
+            infiniteLoop={true}
+            activePosition={'center'}
+            disableSwipe={false}
+            slidesToScroll={1}
+            showSlither={false}
+            firstAndLastGutter={false}
+            requestToChangeActive={setActiveItemIndex}
+            activeItemIndex={activeItemIndex}
+            numberOfCards={Math.min(images.length, 3)}
+            gutter={10}
+            chevronWidth={30}
+            outsideChevron={true}
+            alwaysShowChevrons={false}
+            leftChevron={
+                <img
+                    style={{ ...style.arrow, opacity: leftOpacity }}
+                    src="/arrow-left.png"
+                    alt="left arrow"
+                    onMouseEnter={toggleLeftOpacity}
+                    onMouseLeave={toggleLeftOpacity}
+                />
+            }
+            rightChevron={
+                <img
+                    style={{ ...style.arrow, opacity: rightOpacity }}
+                    src="/arrow-right.png"
+                    alt="right arrow"
+                    onMouseEnter={toggleRightOpacity}
+                    onMouseLeave={toggleRightOpacity}
+                />
+            }
+        >
+            {images.map((image, index) => (
+                <div
+                    key={index}
+                    style={style.imageContainer}
+                >
+                    <img src={image.url} style={style.image} />
+                </div>
+            ))}
+        </ItemsCarousel>
+    )
+}
--- src/components/CommissionCard.jsx
+++ src/components/CommissionCard.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import ItemsCarousel from 'react-items-carousel'
 import { Card } from './Card'
-import { useState } from 'react'
+import { Carousel } from './Carousel'
 
 const style = {
     container: {
@@ -25,83 +24,11 @@ const style = {
         padding: '0 30px',
         maxWidth: 650,
         margin: '0 auto'
-    },
-    imageContainer: {
-        width: 200,
-        height: 290
-    },
-    image: {
-        width: '100%',
-        height: '100%',
-        objectFit: 'cover'
-    },
-    arrow: {
-        width: 50,
-        userSelect: 'none',
-        opacity: 0.25,
-        transition: 'opacity 0.2s'
     }
 }
 
 export const CommissionCard = (props) => {
     const { title, details, images } = props
-    const [activeItemIndex, setActiveItemIndex] = useState(0)
-    const [leftOpacity, setLeftOpacity] = useState(0.25)
-    const [rightOpacity, setRightOpacity] = useState(0.25)
-
-    const toggleLeftOpacity = () => {
-        setLeftOpacity(prevOpacity => prevOpacity === 0.25 ? 1 : 0.25)
-    }
-
-    const toggleRightOpacity = () => {
-        setRightOpacity(prevOpacity => prevOpacity === 0.25 ? 1 : 0.25)
-    }
-
-    const carousel = images.length > 0 && (
-        <ItemsCarousel
-            infiniteLoop={true}
-            activePosition={'center'}
-            disableSwipe={false}
-            slidesToScroll={1}
-            showSlither={false}
-            firstAndLastGutter={false}
-            requestToChangeActive={setActiveItemIndex}
-            activeItemIndex={activeItemIndex}
-            numberOfCards={Math.min(images.length, 3)}
-            gutter={10}
-            chevronWidth={30}
-            outsideChevron={true}
-            alwaysShowChevrons={false}
-            leftChevron={
-                <img
-                    style={{ ...style.arrow, opacity: leftOpacity }}
-                    src="/arrow-left.png"
-                    alt="left arrow"
-                    onMouseEnter={toggleLeftOpacity}
-                    onMouseLeave={toggleLeftOpacity}
-                />
-            }
-            rightChevron={
-                <img
-                    style={{ ...style.arrow, opacity: rightOpacity }}
-                    src="/arrow-right.png"
-                    alt="right arrow"
-                    onMouseEnter={toggleRightOpacity}
-                    onMouseLeave={toggleRightOpacity}
-                />
-            }
-        >
-            {images.map((image, index) => (
-                <div
-                    key={index}
-                    style={style.imageContainer}
-                >
-                    <img src={image.url} style={style.image} />
-                </div>
-            ))}
-        </ItemsCarousel>
-    )
-
 
     return (
         <div style={style.container}>
@@ -112,7 +39,9 @@ export const CommissionCard = (props) => {
                         {details}
                     </div>
                     <div style={style.carousel}>
-                        {carousel}
+                        {images.length > 0 && (
+                            <Carousel images={images} />
+                        )}
                     </div>
                 </div>
             </Card>
